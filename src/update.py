77bfe8dcf0ad1e278b183ce6667d53d6b637fd2c
Paul Macdonnell <pgmac@pgmac.net>
2025-06-10 06:56:55 +1000

    Get my links from Link Ace instead. Deprect Pocket now that it's dying :(

--- src/update.py
+++ src/update.py
@@ -1,9 +1,42 @@
 #!/usr/bin/env python3
 
-from pocket import Pocket, PocketException
+# from pocket import Pocket, PocketException
 from os import environ
 import feedparser
 import requests
+
+
+def links_pop():
+    """Get the newest 10 links from my Link Ace
+    ----------- Creates a string of HREF's to insert into README.md
+    Returns:
+        str: A formatted string of links
+    """
+
+    api_url = "https://links.pgmac.net.au/api/v2/links"
+    headers = {
+        'Authorization': f"Bearer {environ.get('PGLINKS_KEY')}",
+        'accept': 'application/json'
+    }
+    params = {
+        'per_page': 10,
+        'order_by': 'created_at',
+        'order_dir': 'desc'
+    }
+    try:
+        response = requests.get(api_url, timeout=30, headers=headers, params=params)
+        response.raise_for_status()
+        for link in response.json().get('data', []):
+            link_id = link.get('id')
+            if not link_id:
+                continue
+            title = link.get('title', 'No Title')
+            url = link.get('url', '#')
+            yield f"* [{title}]({url})"
+    except requests.RequestException as e:
+        print(f"Error fetching links from API: {e}")
+        return {}
+    return {}
 
 
 def pocket_pop():
@@ -80,7 +113,8 @@ def write_file(l_string, l_file):
 readme = ""
 if __name__ == "__main__":
     readme = add_file("src/HEADER.md")
-    readme += pocket_pop()
+    readme += '\n'.join([str(x) for x in links_pop()])
+    # readme += pocket_pop()
     readme += ghstars_pop('pgmac')
     readme += pgmac_pop('https://pgmac.net.au/feed.xml')
     readme += add_file("src/FOOTER.md")
